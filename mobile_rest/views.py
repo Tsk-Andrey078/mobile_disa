3ff6a5584a6fca02eac47c3890139ff4873591e5
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-01-09 16:12:18 +0500

    Update views.py

--- mobile_rest/views.py
+++ mobile_rest/views.py
@@ -1,8 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from fcm_django.models import FCMDevice
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.contrib.auth.hashers import make_password
 from sentry_sdk import capture_exception
