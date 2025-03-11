5ec61448c1c0648b9167debe4b4dc7df93042ac5
Andrey <46263648+Tsk-Andrey078@users.noreply.github.com>
2025-03-11 12:39:51 +0500

    Update admin.py

--- mobile_rest/admin.py
+++ mobile_rest/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, MediaFiles
+from .models import CustomUser, MediaFiles, MediaFile
 admin.site.register(CustomUser)
 admin.site.register(MediaFiles)
+admin.site.register(MediaFile)
