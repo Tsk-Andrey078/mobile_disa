f0cf4924ec64e73879824f5604fc1d5e03e2a9a6
Tsk-Andrey078 <tskandrey2002.@gmail.com>
2025-05-13 11:23:44 +0500

    ADdnews

--- mobile_rest/admin.py
+++ mobile_rest/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, MediaFiles, MediaFile
+from .models import CustomUser, MediaFiles, MediaFile, MediaFileNews, News
 admin.site.register(CustomUser)
 admin.site.register(MediaFiles)
 admin.site.register(MediaFile)
+admin.site.register(News)
+admin.site.register(MediaFileNews)
